f291ca72bd663fa7c90ea204ddb1a882db6ffea6
bolladaniele <bolladaniele@gmail.com>
2024-03-03 13:53:49 +0000

    Setup tailwind.css and add eslint-vue-accessibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2024: true,
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    "plugin:vuejs-accessibility/recommended",
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  plugins: ["vuejs-accessibility"],
+rules: {
+  "vuejs-accessibility/rule-name": "error",
+  "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+},
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts}",
+  ],  theme: {
+    extend: {},
+    colors: {
+      blue:"#2E3A8C",
+      green:"#3B755F",
+      beige:"#F2EBDB",
+      grey:"#F9F9F9"
+    },
+  },
+  plugins: [],
+}
+
